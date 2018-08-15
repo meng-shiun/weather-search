d7aac4ab39df81f56f31d356187e308015078af5
meng-Shiun <lin.mengshiun@gmail.com>
2018-08-15 12:26:17 +0200

    feat: Create Chart Component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Sparklines, SparklinesLine } from 'react-sparklines'
+import Chart from '../components/chart'
 
 class WeatherList extends Component {
   renderWeather = (cityData) => {
     if (!cityData) return
     const name = cityData.city.name
     const temps = cityData.list.map(weather => weather.main.temp)
+    const pressures = cityData.list.map(weather => weather.main.pressure)
+    const humidities = cityData.list.map(weather => weather.main.humidity)
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Sparklines data={temps} width={180} height={100}>
-            <SparklinesLine color="#559fe0" />
-          </Sparklines>
-        </td>
+        <td><Chart data={temps} color="#488fb8" units="K"/></td>
+        <td><Chart data={pressures} color="#e09524" units="hPa"/></td>
+        <td><Chart data={humidities} color="#866be2" units="%"/></td>
       </tr>
     )
   }
@@ -26,9 +26,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
